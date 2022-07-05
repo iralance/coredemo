0ae52f8de13a628be62d24311882ca64d186167c
iralance <iralance@163.com>
2022-07-05 14:42:44 +0800

    init framework

--- main.go
+++ main.go
@@ -21,19 +21,15 @@ func main() {
 		Addr:    ":8080",
 		Handler: core,
 	}
-	// 这个goroutine是启动服务的goroutine
 	go func() {
 		server.ListenAndServe()
 	}()
 
-	// 当前的goroutine等待信号量
 	quit := make(chan os.Signal)
-	// 监控信号：SIGINT, SIGTERM, SIGQUIT
+	// 监控信号：SIGINT, SIGTERM, SIGQUIT ctrl+c kill+\
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	// 这里会阻塞当前goroutine等待信号
 	<-quit
 
-	// 调用Server.Shutdown graceful结束
 	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
